8227d7c5cc1762486af6209d5abe43afeeb7e0b4
charlie112787 <sscharlie1127@gmail.com>
2025-10-06 16:15:47 +0800

    firebase settings

---
+++ web/js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Q74U0IH8xe5ucUnkhzuBY9Inv26SGQc",
+  authDomain: "weather-forecast-c62c3.firebaseapp.com",
+  projectId: "weather-forecast-c62c3",
+  storageBucket: "weather-forecast-c62c3.firebasestorage.app",
+  messagingSenderId: "898186924731",
+  appId: "1:898186924731:web:9bc4884ba3ca070b598f9e",
+  measurementId: "G-KS86ZBCF1K"
+};

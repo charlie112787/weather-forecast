1c0941d1eb656afd4ff1583b730a2f7af7007db3
GodBage123 <imaginezxd@gmail.com>
2025-10-06 16:59:13 +0800

    Update config.py

--- server/config.py
+++ server/config.py
@@ -8,7 +8,7 @@ FIREBASE_SERVICE_ACCOUNT_KEY_PATH = os.path.join(BASE_DIR, "serviceAccountKey.js
 # --- End of Path Configuration ---
 
 # CWA API Authorization Key
-CWA_API_KEY = os.getenv("CWA_API_KEY", "YOUR_CWA_API_KEY_HERE")
+CWA_API_KEY = os.getenv("CWA_API_KEY", "CWA-F3565C7E-B3CB-42AF-B86E-E882A5DAF79F")
 
 # NCDR API Details (User needs to provide these)
 NCDR_API_BASE_URL = os.getenv("NCDR_API_BASE_URL", "YOUR_NCDR_API_URL_HERE")
